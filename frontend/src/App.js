6bf19711459e8a1eb4718387f6843a4bcf6a8549
Simon Benedict <simon.benedict@gmx.de>
2025-04-26 23:53:59 +0200

    Working MVP (no blob stories yet)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,23 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 import _ from "underscore";
-import { Canvas } from '@react-three/fiber';
-import { Blob, floorLevel } from './components/Blob';
-import { Platform } from './components/Platform';
-import { OrbitControls, Html, Text } from '@react-three/drei';
-import { ImportedMesh } from './components/ImportedMesh';
-import { initialize } from './api';
+import { Canvas } from "@react-three/fiber";
+import { Blob, floorLevel } from "./components/Blob";
+import { Platform } from "./components/Platform";
+import { OrbitControls, Html, Text } from "@react-three/drei";
+import { ImportedMesh } from "./components/ImportedMesh";
+import { initialize , getWorldMetrics, wait, proposePolicy} from "./api";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import LinearProgress from "@mui/material/LinearProgress";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import HistoryEduIcon from "@mui/icons-material/HistoryEdu";
-import { IconButton } from '@mui/material';
-import { useThree,useFrame  } from '@react-three/fiber'
-import { useControls } from '@react-three/drei'
-import StoryPopup from './components/StoryPopUp'
+import { IconButton } from "@mui/material";
+import { useThree, useFrame } from "@react-three/fiber";
+import { useControls } from "@react-three/drei";
+import StoryPopup from "./components/StoryPopUp";
 
-const metrics = {
-  happiness: 40,
-  extremism: 20,
-};
+
 
 function HeadlineTicker({ headlines }) {
   const tickerRef = useRef();
@@ -43,21 +40,27 @@ function HeadlineTicker({ headlines }) {
   }, []);
 
   return (
-    <div style={{
-      overflow: "hidden",
-      whiteSpace: "nowrap",
-      width: "100%",
-      background: "darkred",
-      color: "white",
-      fontSize: "1.2rem",
-      padding: "10px 0",
-      // marginTop: "20px",
-      marginBottom:"10px",
-      position: "relative"
-    }}>
+    <div
+      style={{
+        overflow: "hidden",
+        whiteSpace: "nowrap",
+        width: "100%",
+        background: "darkred",
+        color: "white",
+        fontSize: "1.2rem",
+        padding: "10px 0",
+        // marginTop: "20px",
+        marginBottom: "10px",
+        position: "relative",
+      }}
+    >
       <div
         ref={tickerRef}
-        style={{ display: "inline-block", paddingLeft: "100%", whiteSpace: "nowrap" }}
+        style={{
+          display: "inline-block",
+          paddingLeft: "100%",
+          whiteSpace: "nowrap",
+        }}
       >
         {[...headlines, ...headlines].map((headline, idx) => (
           <span key={idx} style={{ marginRight: "50px" }}>
@@ -69,61 +72,70 @@ function HeadlineTicker({ headlines }) {
   );
 }
 
+const generateCleanBlobStories = (numBlobs) => {
+  return new Map(
+    Array.from({ length: numBlobs }).map((_, index) => [index, "No story available"])
+  )
+}
+
 function App() {
   // const [numBlobs, setNumBlobs] = useState(0);
   const blobRefs = useRef([]);
-  const [world, setWorld] = useState({})
-  const numBlobs = world.blobs?.length
-  
-  const headlines = [
-    "Breaking: Blobtopia reaches 40% happiness!",
-    "New Policy Proposed: Less Trash!",
-    "Cyan Blob Moves East!",
-    "Hotpink Blob Dances!",
-    "City Expansion Underway...",
-    "Pollution Levels Drop by 5%",
-  ];
-
+  const [initiliazeDict, setInitializeDict] = useState({});
+ 
+  const numBlobs = initiliazeDict.blobs?.length;
+  const [blobStories, setBlobStories] = useState(generateCleanBlobStories(numBlobs))
   // Function to generate random target position (x, y, z)
   const generateRandomPosition = () => {
     return [Math.random() * 4.0 - 2.0, floorLevel, Math.random() * 4.0 - 2.0];
   };
+
+
+  useEffect(() => {
+    setGoal(initiliazeDict["metrics"])
+  }, [initiliazeDict])
   
-  const [gameStarted, setGameStarted] = useState(false)
+
+  const [gameStarted, setGameStarted] = useState(false);
   const [color, setColor] = useState("white");
-  const [goal, setGoal] = useState("");
+  const [goal, setGoal] = useState("Get cleanliness to 80%");
   const [goalReached, setGoalReached] = useState(50);
-  const [policy, setPolicy] = useState("")
-  const [story, setStory] = useState("")
+  const [policy, setPolicy] = useState("");
+  const [story, setStory] = useState("");
   const [popupOpen, setPopupOpen] = useState(false);
-  const [headlines, setHeadlines] = useState([])
+  const [headlines, setHeadlines] = useState([]);
+  const [metrics, setMetrics] = useState({})
 
   const setStoryPopUp = (story) => {
     setStory(story);
     setPopupOpen(true);
+  };
+
+  const handleTimeStepDictionary = (dict) => {
+    setMetrics(dict.metrics)
+    setBlobStories(generateCleanBlobStories(numBlobs))
+    console.log("mydict",dict)
+    setBlobStories(dict.event.impacts)
+    setHeadlines([dict.event.headline, dict.event.headline_metrics, ...dict.event.subheadlines ])
   }
 
+  useEffect(() => {
+    setGoalReached(metrics.environment_cleanliness*100)
+  }, [metrics])
+  
   // Function to move a specific blob to a random position by its index
   const handleMoveBlob = (index) => {
     const randomPosition = generateRandomPosition();
-    blobRefs.current[index].moveToPosition(randomPosition);  // Trigger move on the specific blob
+    blobRefs.current[index].moveToPosition(randomPosition); // Trigger move on the specific blob
   };
   
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setGoalReached((oldProgress) => {
-        if (oldProgress === 100) {
-          return 0;
-        }
-        const diff = Math.random() * 10 * (Math.random()-.5)*-1;
-        return Math.min(oldProgress + diff, 100);
-      });
-    }, 500);
+  const iterationDance = () =>  {
+  for (let i = 0; i < numBlobs; i++) {
+    handleMoveBlob(i);
+  }
+}
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+ 
 
   return (
     <div className="App">
@@ -135,82 +147,119 @@ function App() {
           "align-items": "center",
         }}
       >
-        <h1>Goal: {goal}</h1>
+        <h1>Goal: Get cleanliness to 80%</h1>
 
         <Box sx={{ width: "60%" }}>
           <LinearProgress variant="determinate" value={goalReached} />
         </Box>
-        {gameStarted ?  <Canvas
-          
-          camera={{ position: [-0.0, 2, 10], fov: 50 }}
-          style={{ width: "100%", height: "65vh", overflow: "hidden" }}
-        > 
-        {/* <LogCamera/> */}
-          <Html
-    position={[-3, 4., -4]}
-    style={{
-      color: "white",
-      background: "rgba(0,0,0,0.5)",
-      padding: "10px",
-      borderRadius: "8px",
-      fontFamily: "monospace",
-      fontSize: "14px",
-      pointerEvents: "none",
-      userSelect: "none",
-    }}
-    transform
-    occlude={false}
-  >
-    <div>
-      <div><strong>Blobtopia</strong></div>
-      {Object.entries(metrics).map(([key, value]) => (
-        <div key={key}>
-          {key.charAt(0).toUpperCase() + key.slice(1)}: {value}
-        </div>
-      ))}
-    </div>
-        </Html>
+        {gameStarted ? (
+          <Canvas
+            camera={{ position: [-0.0, 2, 10], fov: 50 }}
+            style={{ width: "100%", height: "65vh", overflow: "hidden" }}
+          >
+            {/* <LogCamera/> */}
+            <Html
+              position={[-3, 4, -5]}
+              style={{
+                color: "white",
+                background: "rgba(0,0,0,0.5)",
+                padding: "10px",
+                borderRadius: "8px",
+                fontFamily: "monospace",
+                fontSize: "14px",
+                pointerEvents: "none",
+                userSelect: "none",
+              }}
+              transform
+              occlude={true}
+            >
+              <div>
+                <div>
+                  <strong>Blobtopia</strong>
+                </div>
+                {Object.entries(metrics).map(([key, value]) => (
+                  <div key={key}>
+                  {(key.charAt(0).toUpperCase() + key.slice(1)).replaceAll("_", " ")}: 
+                  {typeof value === "number"
+                    ? `${Math.floor(value * 100)}%`
+                    : value}
+                </div>
+                ))}
+              </div>
+            </Html>
 
-          <ambientLight intensity={0.3} />
-          <directionalLight position={[2, 5, 2]} color={color} />
-          <Platform
-            color="gray"
-            scale={[1.2, 1, 1.2]}
-            position={[0, 0, -3.4]}
-          />
-          <Platform color="#32CD32" scale={[2, 1, 2]} />
-          <ImportedMesh
-            path="models/city_merged.obj"
-            mtlPath="models/city_merged.mtl"
-            position={[0.4, 0.22, -3.6]}
-            scale={[0.2, 0.25, 0.2]}
-            rotation={[0, -Math.PI / 2, 0]}
-          />
-          {/* <Blob color="lime" initialX={2} /> */}
-
-          <OrbitControls
-            maxPolarAngle={Math.PI / 2 + Math.PI / 9} // 90 + 20 deg
-            minPolarAngle={0}
-            maxAzimuthAngle={Math.PI / 6} // 30 deg
-            minAzimuthAngle={-Math.PI / 6} // -30 deg
-          />
-
-          {/* Dynamically render blobs */}
-          {Array.from({ length: numBlobs }).map((_, index) => (
-            <Blob
-              key={index}
-              ref={(el) => (blobRefs.current[index] = el)} // Dynamically assign refs
-              color={index % 2 === 0 ? 'hotpink' : 'cyan'}
-              initialX={Math.random() * 4.0 - 2.0}
-              initialY={Math.random() * 4.0 - 2.0}
+            <ambientLight intensity={0.3} />
+            <directionalLight position={[2, 5, 2]} color={color} />
+            <Platform
+              color="gray"
+              scale={[1.2, 1, 1.2]}
+              position={[0, 0, -3.4]}
             />
-          ))}
+            <Platform color="#32CD32" scale={[2, 1, 2]} />
+            <ImportedMesh
+              path="models/city_merged.obj"
+              mtlPath="models/city_merged.mtl"
+              position={[0.4, 0.22, -3.6]}
+              scale={[0.2, 0.25, 0.2]}
+              rotation={[0, -Math.PI / 2, 0]}
+            />
+            {/* <Blob color="lime" initialX={2} /> */}
 
-          <OrbitControls maxPolarAngle={Math.PI / 2 + Math.PI / 9} minPolarAngle={0} maxAzimuthAngle={Math.PI / 6} minAzimuthAngle={-Math.PI / 6} />
-        </Canvas>
-<HeadlineTicker headlines={ Array.from({ length: 100 })
-  .map(() => headlines)
-  .flat()} />
+            <OrbitControls
+              maxPolarAngle={Math.PI / 2 + Math.PI / 9} // 90 + 20 deg
+              minPolarAngle={0}
+              maxAzimuthAngle={Math.PI / 6} // 30 deg
+              minAzimuthAngle={-Math.PI / 6} // -30 deg
+            />
+
+            {/* Dynamically render blobs */}
+            {Array.from({ length: numBlobs }).map((_, index) => (
+              <Blob
+                key={index}
+                ref={(el) => (blobRefs.current[index] = el)} // Dynamically assign refs
+                color={index % 2 === 0 ? "hotpink" : "cyan"}
+                initialX={Math.random() * 4.0 - 2.0}
+                initialY={Math.random() * 4.0 - 2.0}
+                story={"no story rn"}
+                showStory={setStoryPopUp}
+              />
+            ))}
+
+            <OrbitControls
+              maxPolarAngle={Math.PI / 2 + Math.PI / 9}
+              minPolarAngle={0}
+              maxAzimuthAngle={Math.PI / 6}
+              minAzimuthAngle={-Math.PI / 6}
+            />
+          </Canvas>
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "65vh",
+            }}
+          >
+            {" "}
+            <Button
+              variant="contained"
+              color="white"
+              onClick={() => {
+                initialize().then((r) => setInitializeDict(r)); 
+                getWorldMetrics().then((r) =>setMetrics(r));
+                setGameStarted(true);
+              }}
+            >
+              Start Game
+            </Button>{" "}
+          </div>
+        )}
+        <HeadlineTicker
+          headlines={Array.from({ length: 100 })
+            .map(() => headlines)
+            .flat()}
+        />
         <div
           style={{
             display: "flex",
@@ -220,7 +269,7 @@ function App() {
             width: "50%",
           }}
         >
-          <Button variant="outlined" style={{ height: "100%" }}>
+          <Button onClick={()=>{iterationDance(); wait().then((r)=>{handleTimeStepDictionary(r)})}} variant="outlined" style={{ height: "100%" }}>
             Wait
           </Button>
           <h3>or</h3>
@@ -229,34 +278,38 @@ function App() {
             onChange={(event) => {
               setPolicy(event.target.value);
             }}
-
             label="New Policy"
             variant="outlined"
-            style={{width:"40vw"}}
+            style={{ width: "40vw" }}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
-                  <IconButton onClick={() => {
-                    for (let i = 0; i < numBlobs; i++) {
-                      handleMoveBlob(i);
-                    }
-                  }
-                  }>  <HistoryEduIcon /></IconButton>
+                  <IconButton
+                    onClick={()=>{iterationDance(); proposePolicy(policy).then((r)=>{handleTimeStepDictionary(r)})}} 
+                  >
+                    {" "}
+                    <HistoryEduIcon />
+                  </IconButton>
                 </InputAdornment>
               ),
             }}
           />
         </div>
-      <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <button onClick={() => initialize().then(r => setWorld(r))}><h1>Initialize</h1></button>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          
+        </div>
+        <StoryPopup
+          open={popupOpen}
+          onClose={() => setPopupOpen(false)}
+          story={story}
+        />
       </div>
-      <StoryPopup open={popupOpen} onClose={() => setPopupOpen(false)} story={story} />
-    </div>
     </div>
   );
 }
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -22,3 +22,33 @@ export async function getBlobInformation() {
     const data = await response.json()
     return data
 }
+
+export async function getWorldMetrics(){
+    const response = await fetch("http://127.0.0.1:8000/world_metrics")
+    const data = await response.json()
+    return data
+}
+
+export async function wait(){
+  const response = await fetch("http://127.0.0.1:8000/run_iteration?temperature=0.7&create_image=false")
+  const data = await response.json()
+  return data
+}
+
+export async function proposePolicy(policy) {
+  const response = await fetch('http://127.0.0.1:8000/propose_policy', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'accept': 'application/json'
+    },
+    body: JSON.stringify({
+      "proposal": policy,
+      "temperature": .7
+    })
+  });
+
+  const data = await response.json();
+  console.log("Done")
+  return data;
+}
