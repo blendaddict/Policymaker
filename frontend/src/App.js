079c44aedb22d9413bcc45794ce6eef991c82675
Simon Benedict <simon.benedict@gmx.de>
2025-04-26 13:09:50 +0200

    Added multiple platforms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,8 @@ function App() {
       <Canvas camera={{ position: [0, 5, 10], fov: 50 }} style={{ width: "60vw", height: "60vh", overflow: "hidden" }}>
         <ambientLight intensity={0.3} />
         <directionalLight position={[2, 5, 2]} color={color} />
-        <Platform color="green" scale={[1, 1, 1]} />
+        <Platform color="green" scale={[1, 1, 1]} position={[0,0,-5]} />
+        <Platform color="green" scale={[2, 1, 2]} />
         <Blob color="hotpink" initialX={2} />
         <Blob color="cyan" initialX={2} />
         {/* <Blob color="lime" initialX={2} /> */}
--- frontend/src/components/Platform.js
+++ frontend/src/components/Platform.js
@@ -1,13 +1,14 @@
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
 import { useLoader } from '@react-three/fiber'
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useMemo } from 'react'
 import * as THREE from 'three'
 import { animated, useSpring } from '@react-spring/three'
 
 export function Platform(props) {
     const obj = useLoader(OBJLoader, '/models/platform.obj');
     const ref = useRef();
-  
-    return <animated.primitive object={obj} ref={ref} {...props} />;
+    const cloned = obj.clone()
+
+    return <animated.primitive object={cloned} ref={ref} {...props} />;
   }
   
